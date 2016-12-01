1cb9a35d3417fc45136e614df6fafc08177cd308
Claus Jørgensen <clausjoergensen91@gmail.com>
2016-12-01 16:38:59 +0100

    Created index and blogpost

---
+++ ElasticConsoleApp/BlogPost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace ElasticConsoleApp
+{
+    [ElasticsearchType(IdProperty = "Id", Name = "blog_post")]
+    public class BlogPost
+    {
+        public Guid? Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: '{0}', Title: '{1}', Body: '{2}'", Id, Title, Body);
+        }
+    }
+}
---
+++ ElasticConsoleApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nest;
+
+namespace ElasticConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            //Creates an index in Elasticsearch
+            var createIndexResponse = client.CreateIndex("index-claus", c => c
+                    .Settings(s => s
+                        .NumberOfShards(5)
+                        .NumberOfReplicas(1)
+                    )
+                    .Mappings(m => m
+                        .Map<BlogPost>(d => d
+                            .AutoMap()
+                        )
+                    )
+                );
+
+            //Creates a blogpost
+            var blogPost = new BlogPost
+            {
+                Id = Guid.NewGuid(),
+                Title = "First blog post",
+                Body = "This is a very long blog post!"
+            };
+
+            var firstId = blogPost.Id;
+
+            //Indexes the blogpost in the index
+            var res = client.Index(blogPost, p => p
+                .Index("index-claus")
+                .Id(blogPost.Id.ToString())
+                .Refresh());
+
+            Console.WriteLine(res.CallDetails);
+
+        }
+    }
+}
